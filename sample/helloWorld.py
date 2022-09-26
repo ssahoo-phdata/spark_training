e7d542d09da17b069e0cf1707b3b330333bc1eed
ssahoo-phdata <ssahoo-phdata>
2022-09-26 05:34:41 +0000

    sampleTest
    Used the notebook for testing.

---
+++ sample/helloWorld.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # This is sample notebook used for testing purpose
+# MAGIC Objective- Print "Hello World"
+
+# COMMAND ----------
+
+print("Hello World")
