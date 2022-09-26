4dbeb1523ddd7db3cbb3ba84a731ebb7191a108d
ssahoo-phdata <ssahoo-phdata>
2022-09-26 05:46:15 +0000

    Created a dev branch & Notebook created for testing.

---
+++ devSample/testPython.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Test Python 
+# MAGIC 
+# MAGIC This notebook is created to test the dev branch
+# MAGIC 
+# MAGIC Objective- Print Hello World & push the branch to master branch
+
+# COMMAND ----------
+
+print("Hello World")
